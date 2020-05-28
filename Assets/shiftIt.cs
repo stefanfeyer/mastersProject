30cd37ebe6f549c2ca83979e1b0092c4478a9cf5
Feyer <stefan@feyer.de>
2020-05-28 19:54:54 +0200

    augexo?

--- Assets/shiftIt.cs
+++ Assets/shiftIt.cs
@@ -7,7 +7,10 @@ public class shiftIt : MonoBehaviour
     public float shiftX;
     public float shiftZ;
     public GameObject source;
+    public bool augExo = false;
     // Start is called before the first frame update
+    public GameObject teacherHipReference;
+    public GameObject teacherZero;
     void Start()
     {
         
@@ -18,7 +21,18 @@ public class shiftIt : MonoBehaviour
     {
         //Vector3 deltaStudentTeacher = studentHip.transform.position - (teacherHip.transform.position - teacherZero.transform.position);
         //this.transform.position = new Vector3(source.transform.position.x + shiftX, source.transform.position.y, source.transform.position.z + shiftZ) - this.transform.position;
+        if (augExo)
+        {
+            Vector3 deltaStudentTeacher = this.transform.position - (teacherHipReference.transform.position -teacherZero.transform.position);
+            this.transform.position = this.transform.position + deltaStudentTeacher;
+            //Vector3 deltaStudentTeacher = this.transform.position - (teacherHipReference.transform.position - teacherZero.transform.position);
+            //this.transform.position = new Vector3(this.transform.position.x + deltaStudentTeacher.x, this.transform.position.y, this.transform.position.z + deltaStudentTeacher.z) - this.transform.position;
+        } else
+        {
         this.transform.position = new Vector3(source.transform.position.x + shiftX, source.transform.position.y, source.transform.position.z + shiftZ);
-        this.transform.rotation = source.transform.rotation;
+        this.transform.rotation = source.transform.rotation;    
+        
+        }
+        
     }
 }
